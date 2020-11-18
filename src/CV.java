80f04919f99f01105dc4283e1904a28e1f053df9
nss <nss1234321@163.com>
2020-11-18 22:25:28 +0800

    简单实现了图片在panel上的显示

--- src/CV.java
+++ src/CV.java
@@ -1,24 +1,56 @@
+import GUI.GraphViz;
 import GUI.Ui;
 import PROJECTS.Function;
 import PROJECTS.Project;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class CV {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Ui ui=new Ui();
         ui.showUI();
 
-        Function fun1=new Function("fun1","none");
-        Function fun3=new Function("fun3","none");
-        ArrayList<Function> a1=new ArrayList<Function>();
-        a1.add(fun3);
-        Function fun2=new Function("fun2","none",);
-        Function fun1=new Function("fun1","none");
+//        Function fun1=new Function("fun1","none");
+//        Function fun3=new Function("fun3","none");
+//        ArrayList<Function> a1=new ArrayList<Function>();
+//        a1.add(fun3);
+//        Function fun2=new Function("fun2","none");
+//        Function fun11=new Function("fun1","none");
+//
+//        Gloubals.project_show.add(new Project());
+//        int a=Gloubals.a1;
 
-        Gloubals.project_show.add(new Project());
-        int a=Gloubals.a1;
-        System.out.println("aaa");
+
+
+
+
+
+
+
+
+
+
+
+//        String dotFormat="a->;b->c;c-d";
+//        GraphViz.createDotGraph(dotFormat,"Dot");
+
+
+//
+//        String fileName="e";
+//        String dotFormat="1->2;1->3;1->4;4->5;4->6;6->7;5->7;3->8;3->6;8->7;2->8;2->5;";
+//        GraphViz gv=new GraphViz();
+//        gv.addln(gv.start_graph());
+//        gv.add(dotFormat);
+//        gv.addln(gv.end_graph());
+//        // String type = "gif";
+//        String type = "png";
+//        // gv.increaseDpi();
+//        gv.decreaseDpi();
+//        gv.decreaseDpi();
+//        File out = new File(fileName+"."+ type);
+//        gv.writeGraphToFile( gv.getGraph( gv.getDotSource(), type ), out );
+
     }
 
 
--- src/GUI/Ui.java
+++ src/GUI/Ui.java
@@ -1,17 +1,11 @@
 package GUI;
 import PROJECTS.Project;
-import javax.swing.*;
-import java.awt.event.*;
-import java.awt.*;
-import javax.swing.*;
-import java.awt.*;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 
 //图形界面
@@ -19,14 +13,77 @@ public class Ui {
     JFrame jf = new JFrame();
 
     public void showUI(){
-        jf.setSize(380,600);
-        jf.setDefaultCloseOperation(3);
+        jf.setLayout(new BorderLayout());
+        jf.setSize(1000,618);
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jf.setLocationRelativeTo(null);
         jf.setTitle("Code Visuable");
 
-        FlowLayout flow = new FlowLayout();
-        jf.setLayout(flow);
-        jf.setVisible(true);
-    }
+//        FlowLayout flow = new FlowLayout();
+//        jf.setLayout(flow);
 
+
+        JPanel panel1 = new JPanel(null);
+        JLabel label03 = new JLabel();
+        label03.setFont(new Font(null, Font.PLAIN, 25));
+        label03.setText("show");
+//        label03.setLocation(300,20);
+        label03.setSize(200, 50);
+        panel1.add(label03);
+
+ //       jf.setContentPane(panel1);
+        MyPanel mp=new MyPanel();
+
+
+ //       jf.setContentPane(mp);
+//        jf.add(mp);
+        Graphics g =mp.getGraphics();
+        mp.paint(g);
+        jf.getContentPane().add("West", panel1);
+        jf.getContentPane().add("Center", mp);
+//        jf.pack();
+        jf.setVisible(true);
+//        String static void main(String[] args)throws Exception{
+//
+//        }
+    }
 }
+
+
+
+//public class Test extends JFrame{
+//    MyPanel mp=null;
+//    public Test(){
+//        mp= new  MyPanel();
+//        this.add(mp);
+//        this.setSize(550,400);
+//        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        this.setVisible(true);
+//    }
+//    public static void main(String[] args){
+//        new Test();
+//    }
+//}
+
+
+
+class MyPanel extends JPanel{
+    Image image= null;
+    public void paint(Graphics g){
+        try {
+            System.out.println("to write");
+            image= ImageIO.read(new File("e.png" ));
+            BufferedImage image2= ImageIO.read(new File("e.png" ));
+            int a=image2.getWidth();
+            int b=image2.getHeight();
+            g.drawImage(image,   0  , 0 , a,b  ,null );
+        }
+        catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println("no write");
+        }
+    }
+}
+
+
